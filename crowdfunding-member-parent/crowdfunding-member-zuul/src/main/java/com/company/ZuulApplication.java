758066b8a99d937ec05ad6ce38ecad5e66edb9e5
yaochunguang <yaochunguang@ava.com.cn>
2021-01-06 19:28:53 +0800

    网关

---
+++ crowdfunding-member-parent/crowdfunding-member-zuul/src/main/java/com/company/ZuulApplication.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author: yaochunguang
+ * @date: 2021-01-06 19:03
+ * @description: 网关
+ **/
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
